0f1ba01ec8ff2b79fc2575a2f4f5c48f2b7160c6
Selena Britez <selenabritez934@gmail.com>
2025-09-14 22:27:51 -0300

    he realizado los controladores de update y delete con sus rutas correspondientes, funcionan las consultas

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,21 +1,20 @@
 import { userModel } from "../models/user.model.js";
 
 export const createUser = async (req, res) => {
-    const {username, email, password} = req.body; //desestructure los datos del body para usarlos dentro de la función.
-
+    const {username, email, password} = req.body;
     try {
         const newUser = await userModel.create({
             username,
             email,
             password
-        })
+        });
         res.status(201).json ({
             ok:true,
             msg: "usuario creado correctamente",
             data: newUser
-        })
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
         return res.status(500).json ({
             ok:false,
             msg: "error interno del servidor"
@@ -25,13 +24,13 @@ export const createUser = async (req, res) => {
 
 export const getAllUser = async (req, res) => {
     try {
-        const users = await userModel.find()
-        res.status(201).json ({
+        const users = await userModel.find();
+        res.status(200).json ({
             ok:true,
             data: users
-        })
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
         return res.status(500).json ({
             ok:false,
             msg: "error interno del servidor"
@@ -43,16 +42,60 @@ export const getAllUser = async (req, res) => {
 export const getUserById = async (req, res) => {
     const { id } = req.params;
     try {
-        const user = await userModel.findById(id)
-        res.status(201).json ({
+        const user = await userModel.findById(id);
+        res.status(200).json ({
             ok:true,
             data: user
-        })
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
         return res.status(500).json ({
             ok:false,
             msg: "error interno del servidor"
         });
     }
 };
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { username } = req.body;
+  try {
+    const updatedUser = await userModel.findByIdAndUpdate(id, { username }, { new: true });
+    res.status(200).json({
+      ok: true,
+      msg: "Usuario actualizado correctamente",
+      data: updatedUser,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Error interno del servidor",
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await userModel.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Usuario no encontrado"
+      });
+    }
+    res.status(200).json({
+      ok: true,
+      msg: "Usuario eliminado con éxito",
+      data: user
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Error interno del servidor",
+    });
+  }
+};
+
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,24 @@
 import { Router } from "express";   
 import {
      createUser,
+     deleteUser,
      getAllUser, 
-     getUserById} from "../controllers/user.controller.js";
+     getUserById,
+     updateUser} from "../controllers/user.controller.js";
 
 export const userRoutes = Router();
 
 //ruta para crear usuario
 userRoutes.post("/user", createUser);
 
+//ruta para traer a todos los usuarios
 userRoutes.get("/users", getAllUser);
 
+//ruta para traer a los user por id
 userRoutes.get("/users/:id", getUserById);
+
+//ruta para actualizar user
+userRoutes.put("/users/:id", updateUser)
+
+//ruta para eliminar a un user pero por su id
+userRoutes.delete("/users/:id", deleteUser)
